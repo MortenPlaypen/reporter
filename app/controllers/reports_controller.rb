f9fcb5accfa3716b09f19a3f0d382335634da797
Morten Lundsby Jensen <morten@playpenlabs.com>
2016-04-03 14:39:49 -0700

    Adding ZD API

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,15 @@
 class ReportsController < ApplicationController
-	before_filter :authenticate_user!
+	before_filter :authenticate_user!, :create_zendesk_client
 
 	def index
 		@content = GetMetrics.get_metrics(current_user)
-		/@content = "HER!"/
-		/binding.pry/
+		#@content = "HER!"
+		#binding.pry
+	end
+
+	def create_zendesk_client
+		# call to get_metrics to create and return instance
+		@zendesk_client = ZendeskClient.instance()
 	end
 
 end
--- app/models/get_metrics.rb
+++ app/models/get_metrics.rb
@@ -1,11 +1,14 @@
-/require 'zendesk_api'/
-/require 'httparty'/
+require 'zendesk_api'
+#require 'httparty'
 
-class GetMetrics
-	/include HTTParty/
-	/include zendesk_api/
+module GetMetrics
+	#include HTTParty
+	#include 'zendesk_api'
 
 	def self.get_metrics(current_user)
+
+		# curl https://playpenlabs.zendesk.com/api/v2/users/me.json -v -u morten@playpenlabs.com:playpenlabs
+
 		ret_hash = {}
 		last_week = 42
 		ret_hash = { :last_week => last_week }
@@ -13,4 +16,47 @@ class GetMetrics
 		return ret_hash
 	end
 
+	#require 'zendesk_api'
+
+	class ZendeskClient < ZendeskAPI::Client
+
+		def self.instance
+			@instance = ZendeskAPI::Client.new do |config|
+			  # Mandatory:
+
+			  config.url = "playpenlabs.zendesk.com" # e.g. https://mydesk.zendesk.com/api/v2
+
+			  # Basic / Token Authentication
+			  config.username = "morten@playpenlabs.com"
+
+			  # Choose one of the following depending on your authentication choice
+			  #config.token = "your zendesk token"
+			  config.password = "playpenlabs"
+
+			  # OAuth Authentication
+			  #config.access_token = "your OAuth access token"
+
+			  # Optional:
+
+			  # Retry uses middleware to notify the user
+			  # when hitting the rate limit, sleep automatically,
+			  # then retry the request.
+			  config.retry = true
+
+			  # Logger prints to STDERR by default, to e.g. print to stdout:
+			  require 'logger'
+			  config.logger = Logger.new(STDOUT)
+			  binding.pry
+			  
+			  # Changes Faraday adapter
+			  # config.adapter = :patron
+
+			  # Merged with the default client options hash
+			  # config.client_options = { :ssl => false }
+
+			  # When getting the error 'hostname does not match the server certificate'
+			  # use the API at https://yoursubdomain.zendesk.com/api/v2
+			end
+		end
+	end
 end
